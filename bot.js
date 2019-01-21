75dfb47af657d534f5f208a29e7d765ffb85d85a
zakrwn <45009413+zakrwn@users.noreply.github.com>
2019-01-21 15:07:51 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on("message", message => {
+   var prefix = ".";
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n `);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+ client.login(process.env.BOT_TOKEN); 
