f00569e83018449d7b856f7a29d9905c1f1b0368
zakrwn <45009413+zakrwn@users.noreply.github.com>
2019-01-21 16:38:08 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-   var prefix = ".";
-client.on('message', message => {//new msg event
-if(!message.channel.guild) return;
-  if(message.content.startsWith(prefix + 'Rainbow')) {//to create the rainbow role
-      let role = message.guild.roles.find('ron')
-    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
-  //start of create role 
-  if(!role){
-    rainbow =  message.guild.createRole({
-   name: "ron",//the role will create name
-   color: "#000000",//the default color
-   permissions:[]//the permissions
- //end of create role
-})
-
-}
-message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
-}})
-
-client.on('ready', () => {//new ready event
-  setInterval(function(){
-      client.guilds.forEach(g => {
-                  var role = g.roles.find('ron');//rainbow role name
-                  if (role) {
-                      role.edit({color : "RANDOM"});
-                  };
-      });
-  }, 5000);//the rainbow time
-}) 
-
+client.on("ready", () => {
+  function lol() {
+    client.guilds.get('520891603771064321').roles.find("ron").setColor("RANDOM");
+  };
+  setInterval(lol, 1600);
+}); 
 
  client.login(process.env.BOT_TOKEN); 
